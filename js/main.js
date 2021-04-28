4f126e684efaa16ee6a45a6ff110038f57db4fbf
David Cook <deathkick4@gmail.com>
2021-04-27 21:56:18 -0700

    Added more validation

--- js/main.js
+++ js/main.js
@@ -3,8 +3,21 @@ window.onload = function () {
     formBtn.onclick = main;
 };
 function main() {
+    resetErrorMessages();
     isTextPresent("first-name", "First name is required");
     isTextPresent("last-name", "Last name is required");
+}
+function resetErrorMessages() {
+    var allSpans = document.querySelectorAll("form span");
+    for (var i = 0; i < allSpans.length; i++) {
+        var currSpan = allSpans[i];
+        if (currSpan.hasAttribute("data-required")) {
+            currSpan.innerText = "*";
+        }
+        else {
+            currSpan.innerText = "";
+        }
+    }
 }
 function isTextPresent(id, errMsg) {
     var txtBox = document.getElementById(id);
--- ts/main.ts
+++ ts/main.ts
@@ -5,8 +5,27 @@ window.onload = function(){
 }
 
 function main():void{
+    resetErrorMessages();
     isTextPresent("first-name", "First name is required");
     isTextPresent("last-name", "Last name is required");
+}
+
+
+/**
+ * Resets all the spans
+ */
+function resetErrorMessages():void{
+    let allSpans = document.querySelectorAll("form span");
+    for(let i = 0; i < allSpans.length; i++){
+        let currSpan = <HTMLElement>allSpans[i];
+
+        if(currSpan.hasAttribute("data-required")){
+            currSpan.innerText = "*";
+        }
+        else{
+            currSpan.innerText = "";
+        }
+    }
 }
 
 /**
